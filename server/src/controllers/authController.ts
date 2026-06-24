a1e312b76feac7f5cfc585ba9cb423cf8f4c686e
omar Allaham <omarlaham246@gmail.com>
2026-06-25 02:24:57 +0300

    feat: add login functionality with email verification and password validation

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -1,7 +1,8 @@
 import { Response, Request } from "express";
-import { registerUser } from "../models/UserModel";
+import { findUserByEmail, registerUser } from "../models/UserModel";
 import jwt, { SignOptions } from "jsonwebtoken";
 import { Role } from "@prisma/client";
+import bcrypt from "bcrypt";
 
 const CreateToken = (id: string, name: string, role: Role) => {
   const secretKey = process.env.SECRET_KEY!;
@@ -36,3 +37,36 @@ export const register = async (req: Request, res: Response) => {
     });
   }
 };
+
+export const login =async (req:Request,res:Response)=>{
+try{
+  const{email,password}=req.body;
+  if(!email || !password){
+    return res.status(400).json({
+        status:"fail",
+        message:"you must fill all fields"
+    })
+  }
+  const user=await findUserByEmail(email);
+  if(!user){
+    throw new Error("Invalid email or password");
+  }
+  if(!user.isVerifide){
+    throw new Error("Please verify your student email before logging in");
+  }
+  const isPasswordValid=await bcrypt.compare(password,user.password);
+  if(!isPasswordValid){
+    throw new Error("Invalid email or password");
+  }
+  const token=CreateToken(user.id,user.name,user.role);
+  return res.status(200).json({
+    status:"success",
+    message:"User logged in successfully",
+    token
+  })
+}catch(err){
+    return res.status(400).json({
+        status:"fail",
+        message:err.message || "there is something wrong please try again!"
+    })
+}};
--- server/src/models/UserModel.ts
+++ server/src/models/UserModel.ts
@@ -1,18 +1,23 @@
 import prisma from "../lib/prisma";
 import bcrypt from "bcrypt";
 
-const findUserByEmail=async(email:string)=>{
+export const findUserByEmail=async(email:string)=>{
     const user =await prisma.user.findUnique({where:{email}})
     return user;
 }
 export const registerUser=async (name:string,email:string,password:string)=>{
-    const existingUser=await findUserByEmail(email);
-    if(!existingUser){
-        throw new Error("Email is already in use");
+    try{
+        const existingUser=await findUserByEmail(email);
+        if(existingUser){
+            throw new Error("Email is already in use");
+        }
+            const saltRounds=10;
+            const hashPassword =await bcrypt.hash(password,saltRounds);
+            const newUser= await prisma.user.create({data:{name,email,password:hashPassword}});
+            const { password: _, ...userWithoutPassword } = newUser;
+            return userWithoutPassword;
+
+    }catch(err){
+        throw new Error(err.message || "there is something wrong please try again!");
     }
-    const saltRounds=10;
-    const hashPassword =await bcrypt.hash(password,saltRounds);
-    const newUser= await prisma.user.create({data:{name,email,password:hashPassword}});
-    const { password: _, ...userWithoutPassword } = newUser;
-    return userWithoutPassword;
 }
